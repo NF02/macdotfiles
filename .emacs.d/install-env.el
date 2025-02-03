ade734b5fe7164edbf9a650550a7a861e4a44194
NF02 <nicoferru190@gmail.com>
2025-02-03 14:58:50 +0100

    emacs package autoinstall
    automation script for emacs configuration installation

---
+++ .emacs.d/install-env.el
@@ -0,0 +1,60 @@
+(defun install-env ()
+  "Environment install"
+  (custom-set-variables 
+   '(package-selected-packages
+     '(
+       auto-dark
+       which-key
+       ;; company mode
+       company
+       company-spell
+       company-auctex
+       company-ctags
+       company-php
+       company-web
+       company-c-headers
+       company-math
+       
+       ;; latex
+       auctex
+       auctex-latexmk
+       cdlatex
+       latex-extra
+       latex-math-preview
+       yatex
+       
+       ;; org mode
+       org-modern
+       org-contrib
+       org-roam
+       org-roam-ui
+       org-tree-slide
+       org-re-reveal
+       org-fragtog
+
+       ;; babel
+       ob-rust
+       
+       ;; (JAVA) processing mode
+       processing-mode
+       processing-snippets
+
+       ;; Docker
+       dockerfile-mode
+       docker-cli
+       docker-compose-mode
+       docker-api
+       
+       ;; other
+       slime
+       powerline
+       cobol-mode
+       pdf-tools
+       elfeed-org
+       octave
+       neotree
+       vterm
+       vterm-toggle
+       )))
+  (package-install-selected-packages nil))
+(install-env)
